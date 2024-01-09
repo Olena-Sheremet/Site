3dfd50c91718bc2617cbdd76c29791afb7b9c873
Olena Sheremet <lenka.sheremet1978@gmail.com>
2024-01-09 13:38:00 +0200

    1

--- src/main/java/com/example/site/repository/AdministrationsRepository.java
+++ src/main/java/com/example/site/repository/AdministrationsRepository.java
@@ -1,10 +1,14 @@
 package com.example.site.repository;
 
 import com.example.site.model.Administration;
+import com.example.site.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface AdministrationsRepository extends JpaRepository<Administration, Integer> {
+    Optional<Administration> findByUser_Id(int userId);
 
+    Administration findByUser(User user);
 }
 
